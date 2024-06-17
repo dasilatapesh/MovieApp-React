1fde5ccae129b1e03c7d95e054ae969c2db0b4b7
Tapesh Dasila <111082667+dasilatapesh@users.noreply.github.com>
2024-06-18 04:52:28 +0530

    added styled component

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,17 +1,55 @@
 import { Component } from "react";
+import styled from "styled-components";
+
+//using styled component
+ const Nav = styled.div`
+        width: 100%;
+        height: 70px;
+        background-color: rgb(47, 47, 47);
+        display: flex;
+        align-items: center;
+        justify-content:space-between;
+        position: relative;
+ `;
+
+ const Title = styled.div`
+        font-size: 30px;
+        color: #fff;
+        font-weight: 600;
+        font-family: "Montserrat", sans-serif;
+        text-transform: uppercase;
+        margin-left: 40px;
+        &:hover{color: rgb(164, 15, 15);}
+ `;
+
+ const CartCount = styled.div`
+        background: ${(props) => props.color};
+        border-radius: 50%;
+        color: white;
+        padding: 1px 7px;
+        position: absolute;
+        top: -7px;  
+        right: -18px;
+        font-size: 15px;
+        font-weight: bold;
+        visibility: ${(props) => (props.show?"visible":"hidden")}
+ `;
 
 class NavBar extends Component {
 
     render(){
         return (
             <>
-                <div className="nav" style={style.nav}>
-                    <div className="title" style={style.title}>Movie-App</div>
+                <Nav>
+                {/* <div className="nav" style={style.nav}> */}
+                    <Title>Movie-App</Title>
+                    {/* <div className="title" style={style.title}>Movie-App</div> */}
                     <div className="navCart" style={style.cartContainer}>
-                        <img src="https://cdn-icons-png.flaticon.com/128/4290/4290854.png" alt="Cart Icon" style={style.cartIcon}/>
-                        <span style={style.cartCount}>0</span>
+                        <img src="https://cdn-icons-png.flaticon.com/128/891/891462.png" alt="Cart Icon" style={style.icon}/>
+                        <CartCount color="red" show={true}>3</CartCount>
+                        {/* <span style={style.cartCount}>0</span> */}
                     </div>
-                </div>
+                </Nav>
             </>
         );
     }
@@ -38,18 +76,18 @@ const style = {
         fontWeight: 600,
         fontFamily: '"Montserrat", sans-serif',
         textTransform: "uppercase",
-        marginLeft: 20
+        marginLeft: 40
       },
 
       cartContainer: {
         position: "relative",
-        cursor: "pointer"
+        cursor: "pointer",
+        marginRight:40,
       },
       
-      cartIcon:{
+      icon:{
         height: 48,
         width: 40,
-        marginRight:20,
       },
       
       cartCount:{
